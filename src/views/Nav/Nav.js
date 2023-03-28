cd29ae5b22e5e19b3c052bf5cf2d3faf5f276046
lucnd <luc46k1@gmail.com>
2023-03-28 22:30:08 +0700

    Links & NavLinks Trong React.JS Routers phần 2

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./Nav.scss";
+import { NavLink, Link } from "react-router-dom";
 
 class Nav extends React.Component {
   render() {
     return (
       <>
         <div className="topnav">
-          <a className="active" href="/">
+          {/* <Link to="/">Home</Link>
+          <Link to="/todo">Todo App</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/about">About</Link> */}
+
+          <NavLink to="/" exact>
             Home
-          </a>
-          <a href="/todo">Todo App</a>
-          <a href="/contact">Contact</a>
-          <a href="/about">About</a>
+          </NavLink>
+          <NavLink to="/todo">Todo App</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/about">About</NavLink>
         </div>
       </>
     );

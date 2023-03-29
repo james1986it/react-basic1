3891800cb6552f9d1161d09710ac66fc06892a22
james1986it <james1986.it@gmail.com>
2023-03-29 16:33:15 +0700

    Import image

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
+// import ảnh
+import image1 from "../../assets/images/images.jpg";
+// import image2 from "../../assets/images/image2.jpg";
+
 class Home extends React.Component {
   componentDidMount() {
-    setTimeout(() => {
-      console.log("check timeout");
-      // Thực hiện sau 3s sẽ tự chuyển hướng sang trang Todo app
-      this.props.history.push("/todo");
-    }, 3000);
+    // setTimeout(() => {
+    //   console.log("check timeout");
+    //   // Thực hiện sau 3s sẽ tự chuyển hướng sang trang Todo app
+    //   this.props.history.push("/todo");
+    // }, 3000);
   }
   render() {
     console.log(">>> check props của component Home: ", this.props);
     return (
       <>
         <div>Đây là trang chủ</div>
+        <div>
+          <img src={image1} alt="" />
+        </div>
       </>
     );
   }
